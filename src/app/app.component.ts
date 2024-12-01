c6c7a1159eda86692f8f5daab8422bac79de7031
Apsara <apsaraamri@gmail.com>
2024-12-01 11:16:14 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { headerComponent } from './header/header.component';
+import { TopHeaderComponent } from './top-header/top-header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,headerComponent,TopHeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'AngularEKart';
+  title = 'EKart';
 }
